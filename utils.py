4faec31acb4b0b67ab387c166928145d85b80ad9
Yuanchun Li <yuancli@microsoft.com>
2020-08-12 06:36:27 +0800

    refine modeldiff prompt

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import time
 import argparse
 from pdb import set_trace as st
 import json
+import functools
 
 import torch
 import numpy as np
@@ -152,3 +153,64 @@ def advtest_fast(model, loader, adversary, args):
     del test_loader
     del advDataset
     return float(top1_clean)/total*100, float(top1_adv)/total*100, float(adv_trial-adv_success) / adv_trial *100
+
+
+def lazy_property(func):
+    attribute = '_lazy_' + func.__name__
+
+    @property
+    @functools.wraps(func)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, func(self))
+        return getattr(self, attribute)
+
+    return wrapper
+
+
+class Utils:
+    _instance = None
+
+    def __init__(self):
+        self.cache = {}
+
+    @staticmethod
+    def _get_instance():
+        if Utils._instance is None:
+            Utils._instance = Utils()
+        return Utils._instance
+
+    @staticmethod
+    def show_images(images, labels, title='examples'):
+        plt.figure(figsize=(10,10))
+        plt.subplots_adjust(hspace=0.2)
+        for n in range(25):
+            plt.subplot(5,5,n+1)
+            img = images[n]
+            img = img.numpy().squeeze()
+            plt.imshow(img)
+            plt.title(f'{labels[n]}')
+            plt.axis('off')
+        _ = plt.suptitle(title)
+        plt.show()
+
+    @staticmethod
+    def copy_weights(source_model, target_model):
+        # print(source_model.summary())
+        # print(target_model.summary())
+        for i, layer in enumerate(target_model.layers):
+            if not layer.get_weights():
+                continue
+            source_layer = source_model.get_layer(layer.name)
+            # print(layer)
+            # print(source_layer)
+            layer.set_weights(source_layer.get_weights())
+        return target_model
+
+    @staticmethod
+    def normalize(v):
+        norm = np.linalg.norm(v)
+        if norm == 0:
+            return v
+        return v / norm
+
